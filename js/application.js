ef2a059c5b1007ad6a1e5887b6af40af9ff0977c
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-10-13 14:08:05 +0800

    Two players enabled. Added Pikachu

--- js/application.js
+++ js/application.js
@@ -1,50 +1,71 @@
 $(document).ready(function(){
 
-  //Create columns for P1 & P2
-  playerA = new NewSeq();
-  // playerB = new NewSeq();
-  console.log(playerA.sequence);
+  // var Key = [37,40,39,65,83,68];
 
   //Initialize box colors
-  var initializeBlocks = function(player){
-    player.sequence.forEach(function(color, idx){
-      $("#right-column div").eq(-(idx+1)).css("background-color",color);
-    })
+  var initializeBlocks = function(col){
+    col.sequence.forEach(function(color, idx){
+      $(col.column+" div").eq(-(idx+1)).css("background-color",color);
+    });
   };
 
-  initializeBlocks(playerA);
-
-
-
   //Handling Keyboard Input
-  var colPosition = 0;
-  document.onkeydown = function(e){
-    // based on keycode:
-    // identify which player it corresponds
-    // call player.checkAnswer()
-      //if right, move column down by 50px
-      //if incorrect, console.log the color to pick
-    var Color = getColor(e.keyCode)
-
-    if (Color && playerA.checkAnswer(Color)) {
-
-      console.log('got it right!! ' + playerA.sequence.length + ' to go');
-      colPosition = colPosition+50;
-      console.log(colPosition);
-      $("#right-column").css("transform",'translateY('+colPosition+'px)');
-      // $("#right-column div").eq(-1).remove();
-
+  $(document).on("keydown", function(e){
+    var keyColor = getColor(e.keyCode);
+    var column = whichC(e.keyCode);
+    var player = whichP(e.keyCode);
+    if (keyColor && column.checkAnswer(keyColor)) {
+      console.log('got it right!! ' + column.sequence.length + ' to go'); //remove console.log
+      player.colPosition = player.colPosition+50;
+      $(column.column).css("transform",'translateY('+player.colPosition+'px)');
+      player.playerColor.push(keyColor)
     } else {
-      console.log(playerA.sequence[0]);
+      console.log(column.sequence[0]);
+    }
+  });
+
+  var getColor = function(keycode){
+    return playerColor[keycode];
+  };
+
+  var whichC = function(keycode){
+    if (keycode <50) {
+      return colA;
+    } else {
+      return colB;
     }
   };
 
-    var getColor = function(keycode){
-    // LEFT-RED, DOWN-GREEN, RIGHT-BLUE
-    var Key = [37,40,39];
-    return boxColor[Key.indexOf(keycode)];
+  var whichP = function(keycode){
+    if (keycode < 50) {
+      return playerA;
+    } else {
+      return playerB;
+    }
   };
 
+  //Resizing Pikachus
+  $(window).on("resize", function () {
+    var pikaHeight1 = $('#Pikachu1').width() * 1.44;
+    $('#Pikachu1').height(pikaHeight1+"");
+  })
 
+    $(window).on("resize", function () {
+    var pikaHeight2 = $('#Pikachu2').width() * 1.44;
+    $('#Pikachu2').height(pikaHeight2+"");
+  })
+
+  //Init function
+  var init = function(){
+  colA = new Seq("#right-column");//create column colors
+  console.log(colA.sequence);//remove later
+  initializeBlocks(colA);//push column colors to css
+  colB = new Seq("#left-column");
+  initializeBlocks(colB);
+  playerA = new Player("#right-column", colPosition)
+  playerB = new Player("#left-column", colPosition)
+}
+
+  init();
 
 });
--- js/game.js
+++ js/game.js
@@ -1,28 +1,41 @@
 // var Color = ['LEFT-RED','DOWN-GREEN','RIGHT-BLUE'];
 
-var boxColor = ['red','green','blue'];
+//Declare global variable:
+var colPosition = 0;
 
-//Define new Array for P1 & P2 columns
-var NewSeq = function (){
+var playerColor = {
+  37:'red',
+  40:'green',
+  39:'blue',
+  65:'red',
+  83:'green',
+  68:'blue'};
 
+//Define new Array for columns
+var Seq = function (column){
+  this.time_limit = 0;
+  this.column = column;
   this.sequence = [];
   for (var i=0; i<20; i++){
     this.sequence.push( this.randomColor() );
   };
-
-  this.time_limit = 0;
 };
 
-//Define new methods for P1 and P2 Arrays
-
-NewSeq.prototype.randomColor = function(){
-  return boxColor[ Math.floor(Math.random()*3) ];
+Seq.prototype.randomColor = function(){
+  var boxColor = ['red','green','blue','red','green','blue'];
+  return boxColor[ Math.floor(Math.random()*6) ];
 }
 
-NewSeq.prototype.checkAnswer = function(color){
+Seq.prototype.checkAnswer = function(color){
   if (color == this.sequence[0]){
     return this.sequence.shift();
   } else {
     return false;
   }
+}
+
+var Player = function (playerCol, colPosition){
+  this.playerCol = playerCol;
+  this.colPosition = colPosition;
+  this.playerColor = [];
 }
