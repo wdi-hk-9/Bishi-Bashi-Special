7a7abf4c4ec245b375dfc5e6a5dc4d8e3e7ac7d7
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-10-14 16:13:11 +0800

    buttons binded and sounds fixed. Stopwatch to be added

--- js/application.js
+++ js/application.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
-//Buzz
+
+//Buzz -register sounds and error alert
   var bgmusic = new buzz.sound("./sounds/juicy_breakout-theme.mp3");
-  var dropboxmusic = new buzz.sound("./sounds/ball-wall.mp3");
-
-  bgmusic.loop().fadeIn(2000).play();
-
+  var kickBox = new buzz.sound("./sounds/ball-wall.mp3");
+  var shakeSound = new buzz.sound("./sounds/pling2.mp3");
+  var combo = new buzz.sound("./sounds/ball-paddle.mp3");
+  var pika = new buzz.sound("./sounds/pikapika.wav");
   if (!buzz.isMP3Supported()) {
     alert ("Your browser doesn't support MP3 Format.")
   };
 
-// button to mute and unmute (buzz)
-  $("#mute").on("click", function (){
-    bgmusic.toggleMute();
-  });
+//BUZZ -set Volume for sounds
+  bgmusic.setVolume(5);
+  kickBox.setVolume(100);
+  combo.setVolume(100);
+  pika.setVolume(100);
+
+  // bgmusic.loop().play();
 
   //Initialize box colors
-  var initializeBlocks = function(column){
+  var initializeBlocks = function(column, offset){
+    var html = '<div class="blocks col-xs-5 col-xs-offset-' + offset + '">&nbsp</div>';
+    for (var i = 0; i < 20; i++){
+      $(column.columnID).append(html);
+    }
     column.sequence.forEach(function(color, idx){
       $(column.columnID + " div").eq(-(idx+1)).css("background-color",color);
     });
@@ -32,21 +40,30 @@ $(document).ready(function(){
         //shake box
       } else {
         if (keyColor && column.dropColumn(keyColor)) {
+          kickBox.play();//play sound
           $(column.columnID).find("div:last-child").remove();//remove column last div
           player.colPosition = player.colPosition+70;
-          $(column.columnID).css("transform",'translateY('+player.colPosition+'px)'); //shift column down
+          // $(column.columnID).css("transform",'translateY('+player.colPosition+'px)'); //shift column down
+          $(column.columnID).css("bottom", (1263 - player.colPosition)+'px'); //shift column down
           player.playerColor.push(keyColor); //update Array
-          player.score(); //player score++
-          game.gameOver();
+          player.score();                    //player score++
+          if (player.playerScore%20 == 0){   //PLAY SOUND argument
+            pika.play();
+          } else if (player.playerScore%5 ==0){
+            combo.play();
+          }                                 //PLAY SOUND arguement
+          game.gameOver();                  //check if game over
           $(player.scoreID).html("Current score: "+player.playerScore);
         } else {
-          $(column.columnID).find("div:last-child").effect({effect:"shake", duration: 600});
+          $(column.columnID).find("div:last-child").addClass("animated flash");
+          $(column.columnID).find("div:last-child").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+            $(this).removeClass("animated flash");
+          });
+          shakeSound.play();               //PLAY SOUND
           player.penalty = true;
           setTimeout(function(){player.penalty = false;}, 500);
         }
       }
-    } else {
-      alert ("Game over! Would you like to start a new game?"); //confirm?
     }
   });
 
@@ -55,16 +72,51 @@ $(document).ready(function(){
   };
 
   var whichC = function(keycode){
-    if (keycode >50) { return game.columnA; }
+    if (keycode <100) { return game.columnA; }
     else             { return game.columnB; }
   };
 
   var whichP = function(keycode){
-    if (keycode > 50) { return game.playerA; }
+    if (keycode <100) { return game.playerA; }
     else              { return game.playerB; }
   };
 
-  // //Resizing Pikachus
+// STOPWATCH:
+
+  ////////////////////////////////////////////////////////////////////
+  // GAME LOGIC
+  ////////////////////////////////////////////////////////////////////
+
+  var init = function () {
+    game = new Game();
+    initializeBlocks(game.columnA, 5);
+    initializeBlocks(game.columnB, 2);
+  };
+
+  var reset = function () {
+    console.log("test");
+    delete game;
+    game = new Game();
+    $(".blocks").remove();
+    initializeBlocks(game.columnA, 5);
+    initializeBlocks(game.columnB, 2);
+    $("#left-column, #right-column").css("bottom",'1263px')
+    $("#scoreA, #scoreB").html("Current score: 0");
+  };
+
+// Buttons to mute and unmute (buzz)
+  $("#mute").on("click", function (){
+    bgmusic.toggleMute();
+    kickBox.toggleMute();
+    shakeSound.toggleMute();
+    combo.toggleMute();
+  });
+
+  $("#reset").on("click", reset);
+
+  init();
+
+    // //Resizing Pikachus
   // $(window).on("resize", function () {
   //   var pikaHeight1 = $('#Pikachu1').width() * 1.44;
   //   $('#Pikachu1').height(pikaHeight1+"");
@@ -74,14 +126,5 @@ $(document).ready(function(){
   //   var pikaHeight2 = $('#Pikachu2').width() * 1.44;
   //   $('#Pikachu2').height(pikaHeight2+"");
   // });
-
-
-  ////////////////////////////////////////////////////////////////////
-  // GAME LOGIC
-  ////////////////////////////////////////////////////////////////////
-
-  game = new Game();
-  initializeBlocks(game.columnA);
-  initializeBlocks(game.columnB);
 
 });
